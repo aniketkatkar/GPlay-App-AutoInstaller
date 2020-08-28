a4d5ed9e7853e7d6d9585942ab8c7b9d9232fb1f
aniketkatkar <locked.ani@gmail.com>
2020-08-28 11:11:30 +0530

    Added airtest and adb commands to automate everything.

--- automate.py
+++ automate.py
@@ -1,7 +1,22 @@
 # -*- encoding=utf8 -*-
+__author__ = "akatkar"
+
+from airtest.core.api import *
+import os
+import time
 import requests
 import bs4
 import play_scraper
+import subprocess
+import logging
+
+#disable debug logs in terminal
+logging.getLogger("airtest").setLevel(logging.CRITICAL)
+
+# connect an android phone with adb
+init_device("Android")
+# or use connect_device api
+connect_device("Android:///TA93300M1A")
 
 #search starts
 #searches and gets the app from playstore
@@ -22,3 +37,37 @@ app_details = play_scraper.details(package_name)               #searches package
 app_size = app_details['size']
 print('Application Size = '+app_size)
 #search ends
+
+auto_setup(__file__)
+
+#launch playstore with specified app
+os.system('adb shell am start -a android.intent.action.VIEW -d market://details?id='+package_name)
+
+#click on install button if it exists and wait till uninstall and open appears
+exists(Template(r"tpl1598367166407.png", record_pos=(0.008, -0.09), resolution=(720, 1280)))
+touch(Template(r"tpl1598367178287.png", record_pos=(-0.001, -0.085), resolution=(720, 1280)))
+#time.sleep(30)
+wait(Template(r"tpl1598367200342.png", record_pos=(0.003, -0.326), resolution=(720, 1280)), timeout=9999999999999)
+
+#open specific application settings and take screenshot. Also import it to local directory and delete from phone
+os.system('adb shell am start -a android.settings.APPLICATION_DETAILS_SETTINGS -d package:'+ package_name)
+time.sleep(5)
+os.system('adb shell screencap -p /sdcard/screen.png')
+os.system('adb pull /sdcard/screen.png ./screen.png')
+os.system('adb shell rm /sdcard/screen.png')
+
+#start capturing logs in file
+myoutput = open('./device_logs.txt','w+')
+p = subprocess.Popen(["adb","logcat"], stdout=myoutput, stderr=subprocess.PIPE, universal_newlines=True)
+time.sleep(3)
+
+#start the application
+os.system('adb shell monkey -p '+ package_name +' -c android.intent.category.LAUNCHER 1')
+#run for specified time ( in seconds )
+time.sleep(180)
+#stop the application
+os.system('adb shell am force-stop '+package_name)
+
+#Go to Homescreen
+os.system('adb shell input keyevent 3')
+print("Task Finished.")
